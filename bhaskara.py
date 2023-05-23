e9a8afe382b54c2f165e7ddcb6a64a0540e3835b
Lucas Rezende Mota <70916102+mota-rezende@users.noreply.github.com>
2023-05-23 13:29:17 -0300

    Add files via upload

---
+++ bhaskara.py
@@ -0,0 +1,15 @@
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+delta = b**2 - 4*a*c
+
+if delta < 0:
+    print("A equação nao possui raizes reais.")
+elif delta == 0:
+    x = -b / 2*a
+    print("A raiz da equação é", x,".")
+else:
+    x1 = (-b + delta**(1/2))/(2*a)
+    x2 = (-b - delta**(1/2))/(2*a)
+    print("As raízes da equação são", x1, "e", x2, ".")
